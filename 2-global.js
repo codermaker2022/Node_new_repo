33085ec0b10a149e9f1f68c2f99b83bf13df4674
codermaker2022 <cvargas.ind@gmail.com>
2022-04-01 17:04:51 -0500

    first commit

---
+++ 2-global.js
@@ -0,0 +1,5 @@
+console.log(__dirname)
+
+setInterval(() => {
+    console.log('Hellow world')
+},1000)
---
+++ 4-names.js
@@ -0,0 +1,9 @@
+
+//public 
+const john= 'john'
+const peter= 'peter'
+
+//secret
+const secret = 'SUPER SECRET'
+
+module.exports = {john,peter}
